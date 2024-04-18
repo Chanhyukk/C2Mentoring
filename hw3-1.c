a56b724d135d8b32da1bbab61875561f68ec5222
Chanhyukk <150499792+Chanhyukk@users.noreply.github.com>
2024-04-18 15:44:53 +0900

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -3,34 +3,43 @@
 #include <string.h>
 
 int main(int argc, char* argv[]) {
-    int row, num1, num2;
+    int row, column, num1, num2;
     row = atoi(argv[1]);
-    int arr[row][5];
     if (strcmp(argv[2], "-r") == 0) {
         num1 = atoi(argv[3]);
         num2 = atoi(argv[4]);
+        column = (num2 - num1 + 1) / row;
+        int arr[row][column];
         for (int i = 0; i < row && num2 > num1; i++) {
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < column; j++) {
                 arr[i][j] = num2;
                 num2--;
             }
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+            }
+            printf("\n");
         }
     }
     else {
         num1 = atoi(argv[2]);
         num2 = atoi(argv[3]);
+        column = (num2 - num1 + 1) / row;
+        printf("%d\n", column);
+        int arr[row][column];
         for (int i = 0; i < row && num1 < num2; i++) {
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < column; j++) {
                 arr[i][j] = num1;
                 num1++;
             }
         }
-    }
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < 5; j++) {
-            printf("%d ", arr[i][j]);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                printf("%d ", arr[i][j]);
+            }
+            printf("\n");
         }
-        printf("\n");
     }
     return 0;
 }
