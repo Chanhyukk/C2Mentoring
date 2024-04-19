20afc3cd7ad0bc650c9613a3744d31672130631e
Chanhyukk <150499792+Chanhyukk@users.noreply.github.com>
2024-04-19 13:22:56 +0900

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char* argv[]) {
+/*int main(int argc, char* argv[]) {
     int row, column, num1, num2;
     row = atoi(argv[1]);
     if (strcmp(argv[2], "-r") == 0) {
@@ -41,5 +41,48 @@ int main(int argc, char* argv[]) {
             printf("\n");
         }
     }
+    return 0;
+}*/
+
+int arrToInt(char* v) {
+    int i = 0;
+    int result = 0;
+    while (v[i] >'0' && v[i] <= '9') {
+        result = result * 10 + (v[i] - '0');
+        i++;
+    }
+    return result;
+}
+
+int main(int argc, char* argv[]) {
+    int row, num1, num2, column;
+    row = arrToInt(argv[1]);
+    num1 = arrToInt(argv[argc - 2]);
+    num2 = arrToInt(argv[argc - 1]);
+    column = (num2 - num1 + 1) / row;
+    int arr[row][column];
+    if (strcmp(argv[2], "-r") == 0) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column && num2 >= num1; j++) {
+                arr[i][j] = num2--;
+            }
+            printf("\n");
+        }
+    }
+    else {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                arr[i][j] = num1++;
+            }
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
