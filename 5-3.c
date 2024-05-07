9a6ee35a44d13dafca998dc26167050ec8803601
Chanhyukk <150499792+Chanhyukk@users.noreply.github.com>
2024-05-07 11:46:31 +0900

    Add files via upload

---
+++ 5-3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int mystrlen(char *s) {
+    int i = 0;
+    while (*s != '\0') {
+        i++;
+        s++;
+    }
+    return i;
+}
+
+int main() {
+    char *s[] = {"This is Lab5", "Easy C Programming", "Have Fun"};
+    printf("with strlen\n");
+    int n = (int) (sizeof(s) / sizeof(char *));
+    for (int i = 0; i < n; i++) {
+        printf("%s %d\n", *(s + i), (int)strlen(s[i]));
+    }
+    putchar('\n');
+    for (int i = 0; i < n; i++) {
+        printf("%s %d\n", *(s + i), mystrlen(s[i]));
+    }
+    return 0;
+}
