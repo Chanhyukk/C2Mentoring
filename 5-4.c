6c5cc626050794fab0d10594d8ddc9ccf785cff1
Chanhyukk <150499792+Chanhyukk@users.noreply.github.com>
2024-05-09 16:59:13 +0900

    Update 5-4.c

--- 5-4.c
+++ 5-4.c
@@ -6,12 +6,14 @@ void b (int *p) {
 }
 
 int main() {
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int a[] = {0, 1, 2,3,4,5,6,7,8,9};
     int n = (int) (sizeof(a) / sizeof(int));
     for (int i = 0; i < n; i++) {
         printf("%d ", a[i]);
     }
     putchar('\n');
     b(a + n - 1);
+    //a[]의 마지막 요소를 가리키는 포인터 전달함
+    //마지막, 마지막 - 1, 마지막 - 2번째 요소 출력
     return 0;
 }
