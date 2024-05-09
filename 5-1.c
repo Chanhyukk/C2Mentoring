02fb52217907ecd624370c7861816f57f3e02cb6
Chanhyukk <150499792+Chanhyukk@users.noreply.github.com>
2024-05-09 16:39:20 +0900

    Update 5-1.c

--- 5-1.c
+++ 5-1.c
@@ -3,10 +3,11 @@
 int main() {
     int a[] = {1, 2, 3, 4, 5};
     int *p = a;
+    printf("For Command Version\n");
     for (int i = 0; i < 5; i++) {
         printf("%d ", a[i]);
     }
-    printf("\n");
+    printf("While Command Version\n");
     while (p - a < 5) {
         printf("%d ", *(p++));
     }
